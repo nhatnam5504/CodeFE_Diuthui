e8841eed5d414435c41056fe0ff503ccd33f3003
Hoang Nam <namhoang0505pro@gmail.com>
2026-01-31 21:04:05 +0700

    code

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             '/api': {
-                target: 'http://localhost:8080',
+                target: 'https://ourlove5504-gtr5j.ondigitalocean.app/',
                 changeOrigin: true
             }
         }
